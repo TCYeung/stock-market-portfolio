7fb52324e32da687c23b582b8efc93ad6812888a
Tsz Chung Yeung <tsz-chung.yeung@mrjaffesclass.com>
2022-04-22 22:12:58 +0000

    Connected backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { Card, CardHeader, CardBody, CardFooter, Button } from 'reactstrap';
-import sampleData from './sampleData';
 import StockList from './StockList';
 
 function App() {
+  
+  const AWS_API_GATEWAY = 'https://omrdkfwqj6.execute-api.us-east-1.amazonaws.com/prod';
+  const AWS_API_GATEWAY_GET_PORTFOLIO = AWS_API_GATEWAY + '/get-portfolio';
   
   // Uncomment setMyName if required, for example, if the name
   // is stored in the DynamoDB
@@ -15,7 +17,33 @@ function App() {
   
   // Retrieve the current stock information when the page first loads
   useEffect(() => {
-    setStocks(sampleData);
+    const options = {
+      method: 'POST',
+      cache: 'default'
+    };
+    
+    fetch(AWS_API_GATEWAY_GET_PORTFOLIO, options)
+      .then(function(response) {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(function(response) {
+        console.log(response);
+        let stockList = response.Items.map(item => {
+          item.ticker = item.ticker.S;
+          item.shares = item.shares.N;
+          item.purchasePrice = item.purchasePrice.N;
+          item.name = item.name.S;
+          return item;
+        })
+        setStocks(stockList);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+
   }, []);
   
   // With the stock data add purchase value, current price
